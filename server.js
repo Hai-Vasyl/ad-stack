c04d380f4f9121ba19732419a3255abcc98d8fa5
Hai-Vasyl <hai.vasyl20@gmail.com>
2021-06-03 13:14:06 +0300

    Fixed index.html

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const startServer = async () => {
       app.use(express.static("client/build"))
 
       app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+        res.sendFile(path.resolve("client", "build", "index.html"))
       })
     }
 
